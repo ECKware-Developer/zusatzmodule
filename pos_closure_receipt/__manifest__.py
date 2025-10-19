6ae0982e1aeebeb6b71565e44fb949f0d7b69761
Daniel Fischer | ECKware GmbH <daniel.klotz@eckware.de>
2025-10-19 11:12:00 +0200

    Update POS resceipt

--- pos_closure_receipt/__manifest__.py
+++ pos_closure_receipt/__manifest__.py
@@ -1,12 +1,14 @@
 {
     "name": "POS Closure Receipt (Tips & Cash)",
     "summary": "Druck eines Abschlussbons mit Trinkgeld- und Barbestandsübersicht im POS",
-    "version": "18.0.1.0.2",
+    "version": "18.0.1.1.0",
     "category": "Point of Sale",
     "author": "ECKware",
     "license": "LGPL-3",
     "depends": ["point_of_sale"],
-    "data": [],
+    "data": [
+        "views/pos_config_views.xml"
+    ],
     "assets": {
         "point_of_sale._assets_pos": [
             "pos_closure_receipt/static/src/js/closure_receipt.js",
