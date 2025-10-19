d4b29138973f47e29c4fce1ed2257f961930dee1
Daniel Fischer | ECKware GmbH <daniel.klotz@eckware.de>
2025-10-19 14:36:06 +0200

    add deposit

---
+++ pos_deposit_auto/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "POS Deposit Auto Lines (v18)",
+    "summary": "Auto-add deposit lines in POS using custom fields (Odoo 18).",
+    "version": "18.0.4.1.0",
+    "category": "Point of Sale",
+    "author": "Custom",
+    "license": "LGPL-3",
+    "depends": ["point_of_sale", "product"],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "pos_deposit_auto/static/src/pos_deposit.js",
+        ]
+    },
+    "installable": True,
+    "application": False
+}
