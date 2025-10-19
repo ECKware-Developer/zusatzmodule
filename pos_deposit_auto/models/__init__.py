bfca193f3da7d0038791c7659f0350177f2e654d
Daniel Fischer | ECKware GmbH <daniel.klotz@eckware.de>
2025-10-19 15:32:37 +0200

    delete

--- pos_deposit_auto/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import product_product
-from . import pos_order
-from . import pos_order_line
--- pos_deposit_auto/models/pos_order_line.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models
-
-class PosOrderLine(models.Model):
-    _inherit = "pos.order.line"
-
-    is_deposit = fields.Boolean(string="Is Deposit", default=False, index=True)
-    linked_main_line_id = fields.Many2one("pos.order.line", string="Linked Main Line", ondelete="set null", index=True)
